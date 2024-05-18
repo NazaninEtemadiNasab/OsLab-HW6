652100b2118e40dbdbf8d96153bcdb74d4d2b443
NazaninEtemadiNasab <138618210+NazaninEtemadiNasab@users.noreply.github.com>
2024-05-18 21:40:20 +0330

    Add files via upload

---
+++ Manitor1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+class Manitor1
+{
+    static object Object1 = new object();
+
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(function);
+            thread.Start(i + 1);
+        }
+        Console.ReadKey();
+    }
+
+    static void function(object pno)
+    {
+        Monitor.Enter(Object1);
+        Console.WriteLine($"process {pno} ...");
+        Thread.Sleep(2000);
+        Console.WriteLine($"process {pno} ...");
+        Monitor.Exit(Object1);
+    }
+}

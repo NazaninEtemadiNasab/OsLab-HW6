bf2a695c2aee89ea76e755bcdc4f3bca3e1f32da
NazaninEtemadiNasab <138618210+NazaninEtemadiNasab@users.noreply.github.com>
2024-05-18 21:41:58 +0330

    Add files via upload

---
+++ Mutex1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+class Mutex1
+{
+    static Mutex mutex = new Mutex();
+
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(function);
+            thread.Start(i + 1);
+        }
+        Console.ReadKey();
+    }
+
+    static void function(object pno)
+    {
+        mutex.WaitOne();
+        Console.WriteLine($"process {pno} ...");
+        Thread.Sleep(2000);
+        Console.WriteLine($"process {pno} ...");
+        mutex.ReleaseMutex();
+    }
+}

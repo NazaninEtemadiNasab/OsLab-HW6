cea733a7f34e46f8e4a2090646cba39e0a05eab9
NazaninEtemadiNasab <138618210+NazaninEtemadiNasab@users.noreply.github.com>
2024-05-18 21:31:46 +0330

    Add files via upload

---
+++ Semaphore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+class Semaphore
+{
+    static System.Threading.Semaphore semaphore = new System.Threading.Semaphore(2, 2); // ظرفیت Semaphore: 2
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(function);
+            thread.Start(i + 1);
+        }
+        Console.ReadKey();
+    }
+
+    static void function(object pno)
+    {
+        semaphore.WaitOne();
+        Console.WriteLine($"process {pno} ...");
+        Thread.Sleep(2000);
+        Console.WriteLine($"process {pno} ...");
+        semaphore.Release();
+    }
+}
+
+
+
+
+

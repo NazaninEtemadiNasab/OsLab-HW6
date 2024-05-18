0e1b94e47fefcc28fbee3abef5fbad3a353f7b1e
NazaninEtemadiNasab <138618210+NazaninEtemadiNasab@users.noreply.github.com>
2024-05-18 21:36:49 +0330

    Add files via upload

---
+++ Lock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+class Lock
+{
+    static object lockObject = new object();
+
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(function);
+            thread.Start(i + 1);
+        }
+        Console.ReadKey();
+    }
+
+    static void function(object pno)
+    {
+        lock (lockObject)
+        {
+            Console.WriteLine($"process {pno} ...");
+            Thread.Sleep(2000);
+            Console.WriteLine($"process {pno} ...");
+        }
+    }
+}

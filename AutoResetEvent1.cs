487d3f067ed3895115e631219f3c02e99f7e074e
NazaninEtemadiNasab <138618210+NazaninEtemadiNasab@users.noreply.github.com>
2024-05-18 21:35:25 +0330

    Add files via upload

---
+++ AutoResetEvent1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+class AutoResetEvent1
+{
+    static AutoResetEvent autoResetEvent = new AutoResetEvent(true);
+
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Thread thread = new Thread(function);
+            thread.Start(i + 1);
+        }
+        Console.ReadKey();
+    }
+
+    static void function(object pno)
+    {
+        autoResetEvent.WaitOne();
+
+        Console.WriteLine($"process {pno} ...");
+        Thread.Sleep(2000);
+        Console.WriteLine($"process {pno} ...");
+
+        autoResetEvent.Set();
+    }
+}
